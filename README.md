d582349b43a980277af73a686cbed1293d8b5f1c
98zi <anghunk@gmail.com>
2023-11-17 21:05:13 +0800

    更新主题

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 ## hugo-themes-meteor
 
-极简hugo主题。
+简约主题。
 
-预览: https://zishu.me
+预览：https://zishu.me
 
 配置文件在 `config.toml`。
 
-评论使用twikoo，目录 `/layouts/partials/comment.html`
-
-![1694808458209](https://imgurl.zishu.me/images/1694808458209.jpg)
+![](https://imgurl.zishu.me/2023/image_82fc48_.png)
--- static/js/main.js
+++ static/js/main.js
@@ -1,63 +1,9 @@
 $(function () {
-  $('.post-content img').addClass('slb');
   $('.post-content iframe').wrap('<div class="iframe"></div>');
-  
-  $('.slb').simplebox({
-    fadeSpeed: 100
-  });
 
-  $(document).on('scroll', function () {
-    var $pageScrollTop = $(this).scrollTop()
-    if ($pageScrollTop > 200) {
-      $('.gotop').addClass('act')
-    } else {
-      $('.gotop').removeClass('act')
-    }
-  })
-
-  $('.gotop').on('click', function () {
-    $('html, body').stop().animate({
-      scrollTop: 0
-    })
-  })
-
-  $('.menu-icon').click(function () {
-    $(this).toggleClass('act');
-    $('.menu .trigger').slideToggle();
-  })
-
-  $('nav ul a').each(function () {
+  $('nav a').each(function () {
     if ($(this).attr('href') == window.location.pathname) {
       $(this).addClass('active');
     }
   })
-
 })
-
-$("#searchTerm").focus();
-
-// 黑夜模式
-function themedark() {
-  var themedark = localStorage.getItem('themedark');
-  if (themedark == 'dark') {
-    $('html').addClass('dark');
-  }
-  $('#theme-toggle').click(function () {
-    $('html').toggleClass('dark');
-    if ($('.dark').length > 0) {
-      localStorage.setItem('themedark', 'dark');
-    } else {
-      localStorage.setItem('themedark', 'light');
-    }
-
-  })
-}
-themedark();
-
-// 显示目录
-function TableOfContents(){
-  if($('#TableOfContents').html() != ''){
-    $('.toc').addClass('act');
-  }
-}
-TableOfContents();
